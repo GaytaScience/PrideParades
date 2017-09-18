38c39df74ebbc4f3d2b5258230785da30747d6b3
Kelsey <campkels@gmail.com>
2017-09-17 21:50:57 -0400

    Added Company Code

---
+++ notebooks/CoCallOuts.R
@@ -0,0 +1,85 @@
+#------------------------------------------------------------------------
+# Pride Parade Company Call Outs
+# Kelsey Campbell - 7/15/2017
+# 
+# Identify long or consistently supportive companies from Pride Parade data
+#------------------------------------------------------------------------
+
+setwd("C:/Users/Kelsey/Desktop/PrideParades/data")
+
+install.packages('readxl')
+install.packages("ggplot2")
+install.packages("dplyr")
+library('readxl')
+library('ggplot2')
+library("dplyr")
+
+# Read in Data
+#-------------------------------------------
+df <- read_excel("Labeled.xlsx", sheet = "Sheet1")
+
+# Subset to only Companies
+companies <- df[ which(df$Category =='Large National Company' | df$Category =='Local/Regional Company'), ]
+
+
+# Split of Large/Local Companies over Time
+#-------------------------------------------
+
+# Quick Plot
+g <- ggplot(companies, aes(Year))
+g + geom_bar(aes(fill = Category)) 
+
+# Counts for each Category by Year
+sum = summarise(group_by(companies,Year, Category),count =n())
+
+ggplot(sum, aes(x = Year, y = count, fill = Category, label = count)) +
+  geom_bar(stat = "identity") +
+  geom_text(size = 3, position = position_stack(vjust = 0.5))
+
+# Percents on each section
+ggplot(companies %>% group_by(Year, Category) %>%
+         summarize(n = n()) %>%    
+         mutate(pct=n/sum(n)),           
+       aes(Year, n, fill=Category)) +
+  geom_bar(stat="identity") +
+  geom_text(aes(label=paste0(sprintf("%1.1f", pct*100),"%")), 
+            position=position_stack(vjust=0.5)) + 
+  scale_x_continuous(breaks=(2011:2017)) +
+  scale_y_continuous(breaks = seq(0, 120, by = 20), name = "Number of Entries")
+
+ggsave('CompanySplit.png', width = 9, height = 6, dpi = 200)
+
+# Sector Breakdown over Time
+#-------------------------------------------
+
+# Counts for each Sector by Year
+sumsector = summarise(group_by(companies,Year, Sector),count =n())
+
+ggplot(data=sumsector, aes(x=Year, y=count, group = Sector, colour = Sector)) +
+  geom_line(size = 1.05) + geom_point( size=2) +
+  scale_x_continuous(breaks=(2011:2017)) +
+  scale_y_continuous(breaks = seq(0, 40, by = 10), name = "Number of Entries")
+
+ggsave('SectorOverTime.png', width = 9, height = 6, dpi = 200)
+
+# Consistent Companies (Large National Only)
+#-------------------------------------------
+
+lncompanies <- df[ which(df$Category =='Large National Company'), ]
+
+groupcnts <- summarise(group_by(lncompanies,Group),count =n())
+
+groupcnts[which(groupcnts$count == 7), ]
+
+# One and Done Companies (Large National Only)
+#-------------------------------------------
+
+# Remove 2017 (1st timer in 2017 might come back)
+lncompanies_pre17 <- df[ which(df$Category =='Large National Company' & df$Year != 2017), ]
+
+groupcnts_pre17 <- summarise(group_by(lncompanies_pre17,Group),count =n())
+
+save <- groupcnts_pre17[which(groupcnts_pre17$count == 1), ]
+
+
+
